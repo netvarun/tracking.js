cde115d9d4696a53d4c3d30dc02d51e79bafe1b0
Mitchell Bryson <mitchell@blendmode.co.uk>
2011-03-01 00:28:51 +0000

    renamed plugin files

--- javascripts/modules/tracking/jquery.waypoints.tracking.js
+++ javascripts/modules/tracking/jquery.waypoints.tracking.js
@@ -1,36 +1,34 @@
 $(function(){
-  
+
   $('footer').waypoint(
     function(event, direction){
       tracking.event({
-        category  : 'Waypoint', 
+        category  : 'Waypoint',
         action    : '_bottom'
       });
-    }, { 
-      offset: function() {
-        return $(window).height() - $(this).height();
-      }, 
+    }, {
+      offset: 'bottom-in-view',
       triggerOnce: true }
   );
 
   $('body').waypoint(
     function(event, direction){
       tracking.event({
-        category  : 'Waypoint', 
+        category  : 'Waypoint',
         action    : '_fold'
       });
-    }, { 
-      offset: '-50%', 
+    }, {
+      offset: '-50%',
       triggerOnce: true }
   );
 
   $('head').waypoint(
     function(event, direction){
       tracking.event({
-        category  : 'Waypoint', 
+        category  : 'Waypoint',
         action    : '_top'
       });
-    }, { 
+    }, {
       triggerOnce: true }
   );
 
